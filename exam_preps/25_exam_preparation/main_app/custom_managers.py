319c6a2b1c5119a52600bba59dcd4a31f25e651f
VladimirSiTozi <132054352+VladimirSiTozi@users.noreply.github.com>
2024-07-29 23:56:25 +0300

    deleting project

--- exam_preps/25_exam_preparation/main_app/custom_managers.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.db.models import Count
-
-
-class TennisPlayerManager(models.Manager):
-    def get_tennis_players_by_wins_count(self):
-        players = self.annotate(wins_count=Count('player_wins')).order_by('-wins_count', 'full_name')
-        return players
